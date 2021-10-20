8669c80ffb48e7fba23bb4db34084c311a936a26
Subhasish Panda <subhasish.panda@adcolony.com>
2021-10-20 10:23:29 +0530

    changing the region from us-west-1 to us-east-1

--- terraform-environments/aws/staging/10-vpc/main.tf
+++ terraform-environments/aws/staging/10-vpc/main.tf
@@ -1,5 +1,5 @@
 locals {
-  aws_region       = "us-west-1"
+  aws_region       = "us-east-1"
   environment_name = "staging"
   tags = {
     ops_env              = "${local.environment_name}"
@@ -38,10 +38,10 @@ module "vpc" {
   source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/vpc?ref=v1.0.24"
 
   aws_region       = local.aws_region
-  azs              = ["us-west-1a", "us-west-1c", "us-west-1d"]
+  azs              = ["us-east-1a", "us-east-1b"]
   vpc_cidr         = "10.0.0.0/16"
-  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets   = ["10.0.101.0/24", "10.0.102.0/24"]
   environment_name = local.environment_name
   cluster_name     = local.environment_name
   tags             = local.tags
