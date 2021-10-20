cc9c6ce81ff4891d4d6621e1dde495a09e511136
Subhasish Panda <subhasish.panda@adcolony.com>
2021-10-20 10:45:29 +0530

    Reverting the extra az change

--- terraform-environments/aws/staging/10-vpc/main.tf
+++ terraform-environments/aws/staging/10-vpc/main.tf
@@ -38,7 +38,7 @@ module "vpc" {
   source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/vpc?ref=v1.0.24"
 
   aws_region       = local.aws_region
-  azs              = ["us-east-1a", "us-east-1b","us-east-1c"]
+  azs              = ["us-east-1a", "us-east-1b"]
   vpc_cidr         = "10.0.0.0/16"
   private_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets   = ["10.0.101.0/24", "10.0.102.0/24"]
