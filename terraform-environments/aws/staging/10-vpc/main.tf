2030853b44c2340615c0aa71cca28d9d318ab214
Subhasish Panda <subhasish.panda@adcolony.com>
2021-10-21 12:36:32 +0530

    VPC with 2 public subnets

--- terraform-environments/aws/staging/10-vpc/main.tf
+++ terraform-environments/aws/staging/10-vpc/main.tf
@@ -37,15 +37,15 @@ provider "aws" {
 module "vpc" {
   source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/vpc?ref=v1.0.24"
 
-  aws_region       = local.aws_region
-  azs              = ["us-west-2a", "us-west-2b"]
-  vpc_cidr         = "10.0.0.0/16"
-  secondary_cidrs   = []
+  aws_region         = local.aws_region
+  azs                = ["us-west-2a", "us-west-2b"]
+  vpc_cidr           = "10.0.0.0/16"
+  secondary_cidrs    = []
   k8s_worker_subnets = []
-  private_subnets  = []
-  public_subnets   = ["10.0.101.0/24", "10.0.102.0/24"]
-  environment_name = local.environment_name
-  cluster_name     = local.environment_name
-  tags             = local.tags
+  private_subnets    = []
+  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
+  environment_name   = local.environment_name
+  cluster_name       = local.environment_name
+  tags               = local.tags
   enable_vpn_gateway = false
 }
